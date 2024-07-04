a5a17430e8236ca4cda3e84f3afcdccd518f42b9
JyothiPrathigudupu <jyothiprathigudupu@gmail.com>
2024-07-04 23:13:46 +0530

    changes

--- mul.py
+++ mul.py
@@ -1,6 +1,13 @@
-m=[3,4,5,4,7]
-mul=0
-for i in m:
-    mul*=i
-    print(i)
+# 2. Write a Python program to multiply all the items in a list
+# Sample Output
+# [3,4,5,4,7]
+
+# m=[3,4,5,4,7]
+# mul=0
+# for i in m:
+#     mul*=i
+#     print(i)
+
+
+
 
